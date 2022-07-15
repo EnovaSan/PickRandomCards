e79ac756fdc2644c56737c5d2f589e98d2af6d1f
Kristina Dortman <101277777+EnovaSan@users.noreply.github.com>
2022-07-16 00:47:22 +0300

    Add project files.

---
+++ PickRandomCards/CardPicker.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PickRandomCards
+{
+    class CardPicker
+    {
+
+        static Random random = new Random();
+
+        public static string[] PickSomeCards(int numberOfCards)
+        {
+            string[] pickedCards = new string[numberOfCards];
+            for (int i = 0; i < numberOfCards; i++) 
+            {
+                pickedCards[i] = RandomValue() + " of " + RandomSuit();
+            }
+            return pickedCards;
+        }
+
+        private static string RandomValue()
+        {
+            int Value = random.Next(1, 14);
+            if (Value == 1) return "Ace";
+            if (Value == 11) return "Jack";
+            if (Value == 12) return "Queen";
+            if (Value == 13) return "King";
+            return Value.ToString();
+        }
+
+        private static string RandomSuit()
+        {
+            int Value = random.Next(1, 5);
+            if (Value == 1) return "Spades";
+            if (Value == 2) return "Hearts";
+            if (Value == 3) return "Diamonds";
+            return "Clubs";
+        }
+    }
+}
